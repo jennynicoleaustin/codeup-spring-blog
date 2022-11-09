5ce617d4bf42cd9fa411637d1725711d7ea81cff
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-09 11:45:54 -0600

    View Exercise - create dice-roll with links

--- src/main/java/com/codeup/codeupspring/PostController.java
+++ src/main/java/com/codeup/codeupspring/PostController.java
@@ -1,7 +1,6 @@
 package com.codeup.codeupspring;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,11 +18,11 @@ public class PostController {
         return "posts.{id}";
     }
 
-    @GetMapping("/posts/create")
-    public String CreatePostForm (Model model) {
-        model.addAttribute("post", new Post)
-        return "createPostForm";
-    }
+//    @GetMapping("/posts/create")
+//    public String CreatePostForm (Model model) {
+//        model.addAttribute("post", new Post)
+//        return "createPostForm";
+//    }
 
     @PostMapping("/posts/create")
     public String submitCreatePostForm () {
---
+++ src/main/java/com/codeup/codeupspring/RollDiceController.java
@@ -0,0 +1,21 @@
+package com.codeup.codeupspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RollDiceController {
+    @GetMapping("/roll-dice/")
+    public String getRollDice () {
+        return "roll-dice";
+    }
+    @GetMapping(path = ("/roll-dice/{guessNum}"))
+    public String rollDiceResult (Model model, @PathVariable int guessNum) {
+        model.addAttribute("guess", guessNum);
+        double diceRoll = (int) (Math.random() * (6 - 1 + 1)) + 1;
+        model.addAttribute("diceRoll", diceRoll);
+        return "roll-dice-result";
+    }
+}
