e2c1f6233b9c277c9c39472ec3dfe1b6fd56208d
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-09 11:55:22 -0600

    View Exercise - roll-dice complete

--- src/main/java/com/codeup/codeupspring/RollDiceController.java
+++ src/main/java/com/codeup/codeupspring/RollDiceController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class RollDiceController {
-    @GetMapping("/roll-dice/")
+    @GetMapping("/roll-dice")
     public String getRollDice () {
         return "roll-dice";
     }
     @GetMapping(path = ("/roll-dice/{guessNum}"))
-    public String rollDiceResult (Model model, @PathVariable int guessNum) {
+    public String rollDiceResult (Model model, @PathVariable double guessNum) {
         model.addAttribute("guess", guessNum);
         double diceRoll = (int) (Math.random() * (6 - 1 + 1)) + 1;
         model.addAttribute("diceRoll", diceRoll);
