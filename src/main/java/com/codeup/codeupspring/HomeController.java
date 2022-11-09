5e2be13944529a37f6d758ccd6a229d78862d2e3
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-09 10:50:15 -0600

    Controller exercise - complete PostController

--- src/main/java/com/codeup/codeupspring/HomeController.java
+++ src/main/java/com/codeup/codeupspring/HomeController.java
@@ -1,7 +1,8 @@
 package com.codeup.codeupspring;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class HomeController {
     @GetMapping("/")
     public String getHome () {
--- src/main/java/com/codeup/codeupspring/MathController.java
+++ src/main/java/com/codeup/codeupspring/MathController.java
@@ -1,10 +1,11 @@
 package com.codeup.codeupspring;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@Controller
 public class MathController {
     @RequestMapping(path = "/add/{num1}/and/{num2}", method = RequestMethod.GET)
     @ResponseBody
