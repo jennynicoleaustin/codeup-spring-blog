6ff6412d2f4ade72939d26827fd2d935c7fda5af
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-09 10:36:32 -0600

    Controller exercise - complete MathController class

--- src/main/java/com/codeup/codeupspring/MathController.java
+++ src/main/java/com/codeup/codeupspring/MathController.java
@@ -8,7 +8,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MathController {
     @RequestMapping(path = "/add/{num1}/and/{num2}", method = RequestMethod.GET)
     @ResponseBody
-    public String addTwoNumbers (@PathVariable int num1, @PathVariable String num2) {
-        return num1 + " plus " + num2 + " equals " + num1 + num2;
+    public String addTwoNumbers (@PathVariable int num1, @PathVariable int num2) {
+        return num1 + " plus " + num2 + " equals " + (num1 + num2);
+    }
+    @RequestMapping(path = "/subtract/{num1}/from/{num2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String subtractTwoNumbers (@PathVariable int num1, @PathVariable int num2) {
+        return num2 + " minus " + num1 + " equals " + (num2 - num1);
+    }
+    @RequestMapping(path = "/multiply/{num1}/and/{num2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String multiplyTwoNumbers (@PathVariable int num1, @PathVariable int num2) {
+        return num2 + " times " + num1 + " equals " + (num1 * num2);
+    }
+    @RequestMapping(path = "/divide/{num1}/by/{num2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String divideTwoNumbers (@PathVariable int num1, @PathVariable int num2) {
+        return num1 + " divided " + num2 + " equals " + (num1 / num2);
     }
 }
