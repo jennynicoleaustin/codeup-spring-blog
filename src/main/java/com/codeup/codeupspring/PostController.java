a65f9f5f90b6db62ebf639354f64a159cf0ff3a4
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-12 13:40:02 -0600

    View Exercise: PostController - add control for single post to show - add control for all posts to index

--- src/main/java/com/codeup/codeupspring/PostController.java
+++ src/main/java/com/codeup/codeupspring/PostController.java
@@ -1,32 +1,43 @@
 package com.codeup.codeupspring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
-    @GetMapping("/posts")
-    public String getPostIndexPage () {
 
+    @GetMapping("/posts")
+    public String getPostIndexPage(Model model) {
+        List<Post> allPosts = new ArrayList<>();
+        Post post = new Post("Day One", "Day One on the blog! Check out what I did! ");
+        Post post2 = new Post("Day two", "Day teo on the blog! Check out what I did! ");
+        allPosts.add(post);
+        allPosts.add(post2);
+        model.addAttribute("posts", allPosts);
         return "/posts/index";
     }
 
     @GetMapping(path = "/show")
-    public String getIndividualPostByID (@PathVariable(required = false) Long id) {
+    public String getIndividualPostByID(Model model, @PathVariable(required = false) Long id) {
         Post firstPost = new Post("Welcome Day", "I so excited you are visiting my blog! I hope you stick around");
+        model.addAttribute("post", firstPost);
         return "posts/show";
     }
 
-//    @GetMapping("/posts/create")
-//    public String CreatePostForm (Model model) {
-//        model.addAttribute("post", new Post)
-//        return "createPostForm";
-//    }
+    //    @GetMapping("/posts/create")
+    //    public String CreatePostForm (Model model) {
+    //        model.addAttribute("post", new Post)
+    //        return "createPostForm";
+    //    }
 
     @PostMapping("/posts/create")
-    public String submitCreatePostForm () {
+    public String submitCreatePostForm() {
         return "redirect:/index";
     }
 }
