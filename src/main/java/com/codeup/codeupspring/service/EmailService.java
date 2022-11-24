2b29f8fe38a9a39115658f0ec02f9b805befce08
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-24 14:01:59 -0600

    Services Exercise Complete

--- src/main/java/com/codeup/codeupspring/service/EmailService.java
+++ src/main/java/com/codeup/codeupspring/service/EmailService.java
@@ -1,15 +1,36 @@
 package com.codeup.codeupspring.service;
 
+import com.codeup.codeupspring.entity.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("EmailService")
 public class EmailService {
+
     @Autowired
-    JavaMailSender emailSender;
+    public JavaMailSender emailSender;
 
     @Value("${spring.mail.from}")
     private String from;
+
+    public void prepareAndSend(Post post, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo(post.getUser().getEmail());
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        try{
+            this.emailSender.send(msg);
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
+
 }
